7c99281fa3f010df7d55989d4543c98f6c10e2df
jeremiahvaughan <jeremiah.t.vaughan@gmail.com>
2024-05-04 22:58:15 -0500

    going through kickstart.nvim and picking things I like

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+-- Reference: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
+-- Book mark: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua#L330
 require("jeremiah")
 
 local undo_tree = '<leader>ut'
@@ -38,6 +40,39 @@ vim.api.nvim_set_keymap('i', '<CR>', '<Esc>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<CR>', '<Esc>', { noremap = true, silent = true })
 -- Replace mode
 vim.api.nvim_set_keymap('!', '<CR>', '<Esc>', { noremap = true, silent = true })
+
+-- Easy exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Visual feedback when yanking text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- Keybinds to make split navigation easier.
+-- --  Use CTRL+<hjkl> to switch between windows
+-- --
+-- --  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 
 vim.api.nvim_set_keymap('n', toggle_trouble, ':TroubleToggle<CR>', { noremap = true, silent = true })
 -- Disabling backspace so I use control + h instead since its closer
--- lua/jeremiah/packer.lua
+++ lua/jeremiah/packer.lua
@@ -13,6 +13,19 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup({
+    -- See `:help gitsigns` to understand what the configuration keys do
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
+        },
+    },
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
@@ -30,7 +43,27 @@ require("lazy").setup({
     'tpope/vim-commentary',
     {
         'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        event = 'VimEnter',
+        dependencies = { 
+            'nvim-lua/plenary.nvim',
+            { -- If encountering errors, see telescope-fzf-native README for installation instructions
+                'nvim-telescope/telescope-fzf-native.nvim',
+
+                -- `build` is used to run some command when the plugin is installed/updated.
+                -- This is only run then, not every time Neovim starts up.
+                build = 'make',
+
+                -- `cond` is a condition used to determine whether this plugin should be
+                -- installed and loaded.
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end,
+            },
+            { 'nvim-telescope/telescope-ui-select.nvim' },
+
+            -- Useful for getting pretty icons, but requires a Nerd Font.
+            { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+        }
     },
     {
         'nvim-treesitter/nvim-treesitter',
