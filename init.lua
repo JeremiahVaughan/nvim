aa43fe66a75d2cfcf78b21fe663e9ec1a61a86e9
Jeremiah Vaughan <46267375+JeremiahVaughan@users.noreply.github.com>
2024-05-27 12:42:00 -0500

    adding awesome

--- init.lua
+++ init.lua
@@ -48,6 +48,10 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Adding shortcut to open lazygit
+vim.api.nvim_set_keymap('n', '<leader>lg', ':te lazygit<CR>', { noremap = true, silent = true })
+
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
