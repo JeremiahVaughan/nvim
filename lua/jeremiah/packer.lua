49e8099d43cf79bc3a9cb44ce9f311f4a94675ed
Jeremiah Vaughan <46267375+JeremiahVaughan@users.noreply.github.com>
2024-06-19 16:03:33 -0500

    removing gitsigns because its throwing errors and I don't think im using it

--- lua/jeremiah/packer.lua
+++ lua/jeremiah/packer.lua
@@ -58,19 +58,6 @@ require("lazy").setup({
             },
         },
     },
-    -- See `:help gitsigns` to understand what the configuration keys do
-    { -- Adds git related signs to the gutter, as well as utilities for managing changes
-        'lewis6991/gitsigns.nvim',
-        opts = {
-            signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-            },
-        },
-    },
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
