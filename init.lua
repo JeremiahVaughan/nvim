85a294f6b0d940613f64683737d3c2dfd0b57859
jeremiah.vaughan <Jeremiah.Vaughan@commscope.com>
2024-08-09 11:36:06 -0500

    trying out ssh clipboard configuration

--- init.lua
+++ init.lua
@@ -400,3 +400,30 @@ vim.api.nvim_create_autocmd('TermOpen', {
 	pattern = '*',
 	command = 'startinsert',
 })
+
+-- Clipboard setup so clipboard works well with SSH sessions
+
+function my_paste(reg)
+	return function(lines)
+		local content = vim.fn.getreg('"')
+		return vim.split(content, '\n')
+	end
+end
+
+if (os.getenv('SSH_TTY') == nil)
+then
+	vim.opt.clipboard:append("unnamedplus")
+else
+	vim.opt.clipboard:append("unnamedplus")
+	vim.g.clipboard = {
+		name = 'OSC 52',
+		copy = {
+			['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+			['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+		},
+		paste = {
+			["+"] = my_paste("+"),
+			["*"] = my_paste("*"),
+		},
+	}
+end
