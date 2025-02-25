a1b573e9ed1c4bd9e385eca7357368b3bbdb88f0
jeremiah.vaughan <Jeremiah.Vaughan@commscope.com>
2025-02-25 09:44:53 -0600

    added buffer text auto complete back in

--- lua/jeremiah/lsp.lua
+++ lua/jeremiah/lsp.lua
@@ -34,36 +34,36 @@
 --     end,
 -- })
 
--- local cmp = require('cmp')
--- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
--- cmp.setup({
---     sources = {
---         { name = 'path' },
---         { name = 'nvim_lsp' },
---         { name = 'nvim_lua' },
---         { name = 'luasnip', keyword_length = 2 },
---         { name = 'buffer',  keyword_length = 3 },
---     },
---     -- formatting = lsp_zero.cmp_format({ details = false }),
---     mapping = cmp.mapping.preset.insert({
---         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
---         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
---         ['<Tab>'] = cmp.mapping.confirm({ select = true }),
---         ['<C-Space>'] = cmp.mapping.complete(),
---     }),
---     -- snippet = {
---     --     expand = function(args)
---     --         require('luasnip').lsp_expand(args.body)
---     --     end,
---     -- },
---     snippet = {
---         expand = function(args)
---             -- You need Neovim v0.10 to use vim.snippet
---             vim.snippet.expand(args.body)
---         end,
---     },
--- })
+cmp.setup({
+    sources = {
+        { name = 'path' },
+        -- { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip', keyword_length = 2 },
+        { name = 'buffer',  keyword_length = 3 },
+    },
+    -- formatting = lsp_zero.cmp_format({ details = false }),
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
+    -- snippet = {
+    --     expand = function(args)
+    --         require('luasnip').lsp_expand(args.body)
+    --     end,
+    -- },
+    snippet = {
+        expand = function(args)
+            -- You need Neovim v0.10 to use vim.snippet
+            vim.snippet.expand(args.body)
+        end,
+    },
+})
 
 
 
