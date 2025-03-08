a4b4f437b0582a89053d57eb2c05d4314f96f0ae
jeremiahvaughan <jeremiah.t.vaughan@gmail.com>
2025-03-07 18:50:51 -0600

    commenting out a missed lsp

--- lua/jeremiah/nvim-http.lua
+++ lua/jeremiah/nvim-http.lua
@@ -1,25 +1,25 @@
-vim.filetype.add({
-    extension = {
-        http = "http", -- Files with `.http` extension will have `http` filetype
-    },
-})
+-- vim.filetype.add({
+--     extension = {
+--         http = "http", -- Files with `.http` extension will have `http` filetype
+--     },
+-- })
 
-local client = vim.lsp.start_client {
-    name = "http_lsp",
-    cmd = { "http-lsp" },
-    on_attach = function()
-        -- todo keybinds here
-    end
-}
+-- local client = vim.lsp.start_client {
+--     name = "http_lsp",
+--     cmd = { "http-lsp" },
+--     on_attach = function()
+--         -- todo keybinds here
+--     end
+-- }
 
-if not client then
-    vim.notify("error, could not locate the http-lsp client", vim.log.levels.ERROR)
-    return
-end
+-- if not client then
+--     vim.notify("error, could not locate the http-lsp client", vim.log.levels.ERROR)
+--     return
+-- end
 
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "http",
-    callback = function()
-        vim.lsp.buf_attach_client(0, client)
-    end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "http",
+--     callback = function()
+--         vim.lsp.buf_attach_client(0, client)
+--     end,
+-- })
