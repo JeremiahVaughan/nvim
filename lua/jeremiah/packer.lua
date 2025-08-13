85817bc38a3c2e72d92585f4d67102e89df2a872
JeremiahVaughan <jeremiah.t.vaughan@gmail.com>
2025-08-13 08:54:12 -0500

    finally fixed the lua warning for hererocks. Turns out rocks configuration is the second argument in the setup function, not the first.

--- lua/jeremiah/packer.lua
+++ lua/jeremiah/packer.lua
@@ -13,12 +13,6 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup({
-    rocks = {
-        -- Disable Lua dependency stuff since external dependencies are not needed right now and hopefully won't be
-        -- Installing these was a HUGE pain in past experience and I'm not looking to become a Lua ecosystem expert
-        enabled = false,
-        hererocks = false,
-    },
     {
         "trixnz/sops.nvim",
         lazy = false
@@ -110,4 +104,13 @@ require("lazy").setup({
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" }
     }
-})
+},
+{
+    rocks = {
+        -- Disable Lua dependency stuff since external dependencies are not needed right now and hopefully won't be
+        -- Installing these was a HUGE pain in past experience and I'm not looking to become a Lua ecosystem expert
+        enabled = false,
+        hererocks = false,
+    },
+}
+)
