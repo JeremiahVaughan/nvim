ed71d7af3a9512f5de76dc6366da646703b0a885
JeremiahVaughan <jeremiah.t.vaughan@gmail.com>
2025-08-13 09:00:16 -0500

    don't think I'm using these, commenting them out for now

--- lua/jeremiah/packer.lua
+++ lua/jeremiah/packer.lua
@@ -83,10 +83,10 @@ require("lazy").setup({
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
-    'saadparwaiz1/cmp_luasnip',
+    -- 'saadparwaiz1/cmp_luasnip',
     -- 'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lua',
-    'L3MON4D3/LuaSnip',
+    -- 'hrsh7th/cmp-nvim-lua',
+    -- 'L3MON4D3/LuaSnip',
     {
         'nvim-lualine/lualine.nvim',
         dependencies = {
--- lua/jeremiah/treesitter.lua
+++ lua/jeremiah/treesitter.lua
@@ -1,11 +1,12 @@
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
+
 cmp.setup({
     sources = {
         { name = 'path' },
-        { name = 'nvim_lua' },
-        { name = 'luasnip', keyword_length = 2 },
+        -- { name = 'nvim_lua' },
+        -- { name = 'luasnip', keyword_length = 2 },
         { name = 'buffer',  keyword_length = 3 },
     },
     mapping = cmp.mapping.preset.insert({
