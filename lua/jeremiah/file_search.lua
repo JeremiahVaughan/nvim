610a37fa94cf2071297ab92d71e80e2090bdcf03
jeremiah.vaughan <Jeremiah.Vaughan@commscope.com>
2025-10-30 14:16:09 -0500

    added search for file and put in quickfix list command as an alternative to telescope, to see how I like it

---
+++ lua/jeremiah/file_search.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+vim.api.nvim_create_user_command('Fd', function(opts)
+    local pattern = opts.args
+    local files = vim.fn.systemlist('fd -t f ' .. pattern)
+    vim.fn.setqflist({}, ' ', {
+        title = 'fd ' .. pattern,
+        items = vim.tbl_map(function(f)
+            return { filename = f }
+        end, files)
+    })
+    vim.cmd('copen')
+end, { nargs = 1 })
+
+return M
