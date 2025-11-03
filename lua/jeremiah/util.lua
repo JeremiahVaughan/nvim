8e5be0483fa889bd7c8ed12ccda9723a56e1ca7f
JeremiahVaughan <jeremiah.t.vaughan@gmail.com>
2025-11-03 08:46:49 -0600

    removing oil buffers from autosave

--- lua/jeremiah/util.lua
+++ lua/jeremiah/util.lua
@@ -1,14 +1,24 @@
 local M = {}
 
-function M.IsTerminalBuffer(buf)
-    local buftype = vim.api.nvim_get_option_value('buftype', { buf = buf })
-    return buftype == 'terminal'
-end
-
 function M.SaveAll()
-    local currentBuf = vim.api.nvim_get_current_buf()
-    if not M.IsTerminalBuffer(currentBuf) and vim.api.nvim_buf_get_name(currentBuf) ~= "" then
-        vim.cmd('wa') -- saving changes so they are in play when the server is launched
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) then
+            local name = vim.api.nvim_buf_get_name(buf)
+            local buftype = vim.api.nvim_get_option_value('buftype', { buf = buf })
+            local filetype = vim.api.nvim_get_option_value('filetype', { buf = buf })
+            local modified = vim.api.nvim_get_option_value('modified', { buf = buf })
+            local modifiable = vim.api.nvim_get_option_value('modifiable', { buf = buf })
+            if name ~= ""
+                and modified
+                and modifiable
+                and buftype ~= 'terminal'
+                and filetype ~= 'oil'
+            then
+                vim.api.nvim_buf_call(buf, function()
+                    vim.cmd('silent write')
+                end)
+            end
+        end
     end
 end
 
