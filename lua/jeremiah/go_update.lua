3a66748b2ab7ab3046410a2ad92754cb4269ce11
jeremiah.vaughan <Jeremiah.Vaughan@commscope.com>
2025-11-11 09:57:03 -0600

    araaaaaa

--- lua/jeremiah/go_update.lua
+++ lua/jeremiah/go_update.lua
@@ -7,7 +7,7 @@ end
 local function resolve_target_version()
     local version = vim.g.go_update_target_version
     if version == nil or version == '' then
-        return '1.24.9'
+        return '1.24.10'
     end
     return tostring(version)
 end
