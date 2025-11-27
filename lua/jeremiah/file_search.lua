119f95b98262d6f60a6721c4484dfb1cea4cd4ae
JeremiahVaughan <jeremiah.t.vaughan@gmail.com>
2025-11-27 12:42:53 -0600

    switching fd search to unrestricted directory search

--- lua/jeremiah/file_search.lua
+++ lua/jeremiah/file_search.lua
@@ -2,7 +2,7 @@ local M = {}
 
 vim.api.nvim_create_user_command('Fd', function(opts)
     local pattern = opts.args
-    local files = vim.fn.systemlist('fd -t f ' .. pattern)
+    local files = vim.fn.systemlist('fd -u ' .. pattern)
     vim.fn.setqflist({}, ' ', {
         title = 'fd ' .. pattern,
         items = vim.tbl_map(function(f)
