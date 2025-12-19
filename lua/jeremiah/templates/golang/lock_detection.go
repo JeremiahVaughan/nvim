5dcc7eaaccc3adfbe6a3da3ad23cee9db13df525
jeremiah.vaughan <Jeremiah.Vaughan@commscope.com>
2025-12-19 11:01:43 -0600

    t

--- lua/jeremiah/templates/golang/lock_detection.go
+++ lua/jeremiah/templates/golang/lock_detection.go
@@ -24,20 +24,20 @@ func (m *HealthModel) CheckDatabaseLock(ctx context.Context, timeout time.Durati
 
 	// Attempt to acquire a write lock immediately
 	// With PRAGMA busy_timeout = 5000, this will wait up to 5 seconds before returning SQLITE_BUSY
-	_, err := m.database.ExecContext(checkCtx, "DELETE FROM health_check_test WHERE id = 999")
+	_, err := m.database..ExecContext(checkCtx, "DELETE FROM health_check_test WHERE id = 999")
 	if err != nil {
 		// Check if the error is a SQLite error with BUSY_TIMEOUT or BUSY code
 		var sqliteErr *sqlite3.Error
 		if errors.As(err, &sqliteErr) {
 			// Check for BUSY_TIMEOUT (extended error code) or BUSY (base error code)
 			if sqliteErr.ExtendedCode() == sqlite3.BUSY_TIMEOUT || sqliteErr.Code() == sqlite3.BUSY {
-				msg := fmt.Sprintf("database has been locked for more than 5 seconds: %w")
+				msg := fmt.Sprintf("database has been locked for more than 5 seconds: %v", err)
 				log.Println(msg)
 				return errors.New(msg)
 			}
 		}
 		// Other errors (connection issues, etc.)
-		msg := fmt.Sprintf("database error: %w", err)
+		msg := fmt.Sprintf("database error: %v", err)
 		log.Println(msg)
 		return errors.New(msg)
 	}
