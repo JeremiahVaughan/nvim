96530d83be1d306b0c3a001cac2a91ad97d2a78d
JeremiahVaughan <jeremiah.t.vaughan@gmail.com>
2026-01-14 18:14:32 -0600

    Revert "trying a new pattern for managing embedded process like lazygit. Opening up a dedicated tab for each so split layouts like dadbod or vim diff don't get destroyed when I navigate to one of these embedded processes"
    This reverts commit 4c0670cc363f0cded2e3fd0d678ca0c684aad6ec.

--- lua/jeremiah/lazydocker.lua
+++ lua/jeremiah/lazydocker.lua
@@ -11,21 +11,9 @@ function FindLazydockerTerminalBuffer()
 	return nil
 end
 
-local function FindLazydockerTerminalWindow(buf)
-	for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
-		for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
-			if vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_buf(win) == buf then
-				return { tab = tab, win = win }
-			end
-		end
-	end
-	return nil
-end
-
 -- Function to open a new terminal buffer and run the server
 function OpenLazydockerTerminal()
-	-- Open a new tab and start a terminal
-	vim.cmd('tabnew')
+	-- Open a new buffer and start a terminal
 	vim.cmd('te lazydocker')
 	vim.b.is_lazydocker_terminal = true
 end
@@ -34,14 +22,7 @@ function ToggleLazyDockerTerminal()
 	jeremiah.utils.SaveAll()
 	local buf = FindLazydockerTerminalBuffer()
 	if buf then
-		local location = FindLazydockerTerminalWindow(buf)
-		if location then
-			vim.api.nvim_set_current_tabpage(location.tab)
-			vim.api.nvim_set_current_win(location.win)
-		else
-			vim.cmd('tabnew')
-			vim.api.nvim_set_current_buf(buf)
-		end
+		vim.api.nvim_set_current_buf(buf)
 	else
 		OpenLazydockerTerminal()
 	end
--- lua/jeremiah/lazygit.lua
+++ lua/jeremiah/lazygit.lua
@@ -11,21 +11,9 @@ function FindLazyGitTerminalBuffer()
     return nil
 end
 
-local function FindLazyGitTerminalWindow(buf)
-    for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
-        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
-            if vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_buf(win) == buf then
-                return { tab = tab, win = win }
-            end
-        end
-    end
-    return nil
-end
-
 -- Function to open a new terminal buffer and run the server
 function OpenLazyGitTerminal()
-    -- Open a new tab and start a terminal
-    vim.cmd('tabnew')
+    -- Open a new buffer and start a terminal
     vim.cmd('te lazygit')
     vim.b.is_lazygit_terminal = true
 end
@@ -34,14 +22,7 @@ function ToggleLazyGitTerminal()
     jeremiah.utils.SaveAll()
     local buf = FindLazyGitTerminalBuffer()
     if buf then
-        local location = FindLazyGitTerminalWindow(buf)
-        if location then
-            vim.api.nvim_set_current_tabpage(location.tab)
-            vim.api.nvim_set_current_win(location.win)
-        else
-            vim.cmd('tabnew')
-            vim.api.nvim_set_current_buf(buf)
-        end
+        vim.api.nvim_set_current_buf(buf)
     else
         OpenLazyGitTerminal()
     end
