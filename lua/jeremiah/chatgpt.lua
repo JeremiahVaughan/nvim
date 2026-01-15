1e807f093aa8107064def38ed83818a742cb6c00
JeremiahVaughan <jeremiah.t.vaughan@gmail.com>
2026-01-14 18:14:28 -0600

    Revert "matching pattern"
    This reverts commit 31f1bbce7a94a731b06a0b543bec2b74deec5596.

--- lua/jeremiah/chatgpt.lua
+++ lua/jeremiah/chatgpt.lua
@@ -13,35 +13,16 @@ end
 
 -- Function to open a new terminal buffer and run the server
 function OpenChatGptTerminal()
-	-- Open a new tab and start a terminal
-	vim.cmd('tabnew')
+	-- Open a new buffer and start a terminal
 	vim.cmd('te /home/linuxbrew/.linuxbrew/bin/codex')
 	vim.b.is_chatgpt_terminal = true
-end
-
-local function FindChatGptTerminalWindow(buf)
-	for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
-		for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
-			if vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_buf(win) == buf then
-				return { tab = tab, win = win }
-			end
-		end
-	end
-	return nil
 end
 
 function ToggleChatGptTerminal()
 	jeremiah.utils.SaveAll()
 	local buf = FindChatGptTerminalBuffer()
 	if buf then
-		local location = FindChatGptTerminalWindow(buf)
-		if location then
-			vim.api.nvim_set_current_tabpage(location.tab)
-			vim.api.nvim_set_current_win(location.win)
-		else
-			vim.cmd('tabnew')
-			vim.api.nvim_set_current_buf(buf)
-		end
+		vim.api.nvim_set_current_buf(buf)
 	else
 		OpenChatGptTerminal()
 	end
