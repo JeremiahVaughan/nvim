591ca3c623fe665e249c46542f4b54dadf4279b3
JeremiahVaughan <jeremiah.t.vaughan@gmail.com>
2026-01-22 20:03:44 -0600

    forked sops plugin so I can encrypt toml files with it

--- lua/jeremiah/plugins/sops.lua
+++ lua/jeremiah/plugins/sops.lua
@@ -1,6 +1,9 @@
 local M = {
     {
-        "trixnz/sops.nvim",
+        -- "trixnz/sops.nvim", -- forked from
+        -- dir = "~/create_git/sops.nvim", -- local development
+        "jeremiahvaughan/sops.nvim", 
+        name = "sops.nvim",
         lazy = false,
     },
 }
